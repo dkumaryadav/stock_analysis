5b760d0ef0700494d5d07d0d00f0a5ee4b4c1f57
dkumaryadav <deepank148@gmail.com>
2020-05-05 16:05:57 -0500

    Update data-download-consumer.py

--- data-download-consumer.py
+++ data-download-consumer.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 13 15:08:15 2020
+@author: deepakkumaryadav
+"""
 import os
 from kafka import KafkaConsumer
 from kafka import KafkaProducer
 
 topic = "data-available"
-
 success_key = b"SUCCESS"
 
 consumer = KafkaConsumer(topic, auto_offset_reset='latest',  group_id=None)
