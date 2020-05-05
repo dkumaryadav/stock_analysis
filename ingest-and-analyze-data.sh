9549c489cf3c8f10677b372af49cb55523e42afe
dkumaryadav <deepank148@gmail.com>
2020-05-05 14:49:01 -0500

    Updated changes after testing

--- ingest-and-analyze-data.sh
+++ ingest-and-analyze-data.sh
@@ -3,15 +3,23 @@
 #Author: Yawei Zhu
 #Date: 05/01/2020
 ################################################################## 
+
 database="StockMarket"
 table="StockData"
-resultsPath="/home/hadoop/stock"
-hive -e "create database $database;"
-hive -e "CREATE TABLE IF NOT EXISTS $database.$table(stock_symbol STRING, stock_name STRING, stock_price DOUBLE, change DOUBLE, change_percentage DOUBLE, 52WHigh DOUBLE, 52WLow DOUBLE, Time DOUBLE ) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' STORED AS TEXTFILE;"
-hive -e "LOAD DATA LOCAL INPATH $1 OVERWRITE INTO TABLE  $database.$table;"
-hive -e "set hive.cli.print.header=true; SELECT * FROM $database.$table ORDER BY change_percentage DESC LIMIT 10;" | sed 's/[\t]/,/g'  > $resultsPath/top.csv
+resultsPath="/home/hadoop/stocks"
+
+echo "Creating Database"
+hive -e "CREATE DATABASE IF NOT EXISTS $database;"
+
+echo "Creating External Table"
+hive -e "CREATE EXTERNAL TABLE IF NOT EXISTS $database.$table(stock_symbol STRING, stock_name STRING, stock_price DOUBLE, change DOUBLE, change_percentage DOUBLE, 52WHigh DOUBLE, 52WLow DOUBLE, Time DOUBLE ) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' LOCATION '$1';"
+
+echo "Finding top 10"
+hive -e "set hive.cli.print.header=true; SELECT * FROM $database.$table ORDER BY change_percentage DESC LIMIT 10;" | sed 's/[\t]/,/g'  > top.csv
 printf "********** Top 10 stocks based on changePercentage value **********\n"
-cat $resultsPath/top.csv
-hive -e "set hive.cli.print.header=true; SELECT * FROM MyProject.stockInfo ORDER BY change_percentage ASC NULLS LAST LIMIT 10;" | sed 's/[\t]/,/g'  > $resultPath/bottom.csv
+cat top.csv
+
+echo "Finding bottom 10"
+hive -e "set hive.cli.print.header=true; SELECT * FROM $database.$table ORDER BY change_percentage ASC NULLS LAST LIMIT 10;" | sed 's/[\t]/,/g'  > bottom.csv
 printf "********** Last 10 stocks based on changePercentage value **********\n"
-cat $resultsPath/bottom.csv
+cat bottom.csv
